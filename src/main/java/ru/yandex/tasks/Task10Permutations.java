4fbe426b6c93277bb0cf24e1865cead8de899aea
Abrashkov Vadim <avad200062@mail.ru>
2023-08-14 20:49:07 +0300

    Permutations

--- src/main/java/ru/yandex/tasks/Task10Permutations.java
+++ src/main/java/ru/yandex/tasks/Task10Permutations.java
@@ -1,14 +1,38 @@
 package ru.yandex.tasks;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Task10Permutations {
+    static List<int []> ansList = new ArrayList<>();
+    static Set<Integer> used = new HashSet<>();
     public static int[][] permutations(int[] numbers) {
         /*
          * Возвращает массив со всеми перестановками массива numbers (до 10 элементов, все по модулю до 10^5)
          */
         // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return null;
+        // solution
+        int[] permut = new int[numbers.length];
+        permute(numbers, permut, 0);
+
+        // make answer array
+        int[][] answer = new int[ansList.size()][];
+        int point = 0;
+        for(var ar: ansList){
+            answer[point++] = ar;
+        }
+        return answer;
+    }
+
+    public static void permute(int[] numbers, int[] permut, int level) {
+        for (int i = 0; i < numbers.length; i++) {
+            if(used.add(numbers[i])) {
+                permut[level] = numbers[i];
+                if (level == numbers.length - 1) ansList.add(Arrays.copyOf(permut, permut.length));
+                if (level < numbers.length - 1)
+                    permute(numbers, permut, level + 1);
+                used.remove(numbers[i]);
+            }
+        }
     }
 
     public static void selfCheck() {
@@ -21,7 +45,8 @@ public class Task10Permutations {
                 {3, 1, 2},
                 {3, 2, 1}
         };
-
+        System.out.println(Arrays.deepToString(permutations(input)));
+        System.out.println(Arrays.deepToString(output));
         assert Arrays.deepEquals(output, permutations(input));
     }
 }
